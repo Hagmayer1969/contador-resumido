d5ccc2ee9626a3db519850afd49be7d41e6b9736
Edson Hagmayer <prhagmayer@gmail.com>
2025-07-16 19:41:36 -0300

    logoca das cores

--- script.js
+++ script.js
@@ -1,17 +1,27 @@
-  let numero = 0 
-        function add() {
-            numero ++
-            render()
-        }
-         function res() {
-            numero = 0
-            render()
-        }
-           function dim() {
-            numero --
-            render()
-        }
-           function render() {
-            document.querySelector("p").innerText = numero
-        }
-        render()
+let numero = 0
+function add() {
+    numero++
+    render()
+}
+function res() {
+    numero = 0
+    render()
+}
+function dim() {
+    numero--
+    render()
+}
+function render() {
+    document.querySelector("p").innerText = numero
+    if (numero > 0) {
+        document.querySelector("p").style.color = "gray"
+    }
+    else if (numero < 0) {
+        document.querySelector("p").style.color = "red"
+
+    }    else {
+    document.querySelector("p").style.color = "white"
+}
+     
+}
+   
